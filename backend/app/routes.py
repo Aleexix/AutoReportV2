10a5bb67a67e3ff87e6ce0cfaffc7d6e2ffd68c0
Aleexix <aorostegui2@gmail.com>
2025-05-12 13:03:41 -0500

    backend en 90%

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -39,6 +39,7 @@ COGNOS_PASS = os.getenv("COGNOS_PASS", "Orostegui1016952406")
 def configure_edge():
     edge_options = EdgeOptions()
     edge_options.use_chromium = True  # Esto es obligatorio
+    edge_options.add_argument("--start-fullscreen")
     edge_options.add_argument("--disable-notifications")
     edge_options.add_experimental_option("prefs", {
         "download.default_directory": DOWNLOAD_FOLDER,
@@ -93,31 +94,222 @@ def run_process():
         driver.find_element(By.XPATH, '//*[@id="password-input"]').send_keys(COGNOS_PASS)
         driver.find_element(By.XPATH, '//*[@id="login-button"]').click()
 
-        # Paso 3: Descarga
-        WebDriverWait(driver, 40).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="com.ibm.bi.content.navigator.myContent"]'))
-        ).click()
         
         WebDriverWait(driver, 50).until(
-            EC.element_to_be_clickable((By.XPATH, '//*[@id="com.ibm.bi.content.navigator.myContent__panel"]/div[2]/div[2]/div/div[2]/div/label/span[2]/div/div[1]/div/a'))
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="com.ibm.bi.content.navigator.teamContent__panel"]/div[2]/div[2]/div/div[1]/div/label[3]/span[2]/div/div[1]/div/a/div/div'))
         ).click()
-        time.sleep(15)  # Esperar manualmente si es necesario
-        iframe = driver.find_element(By.XPATH, '//*[@id="rsIFrameManager_1"]')
-        driver.switch_to.frame(iframe)
+        time.sleep(10)  # Esperar manualmente si es necesario
+        
+         # Ahora puedes acceder al div con scroll
+        scrollable_div = driver.find_element(By.XPATH, '//*[@id="com.ibm.bi.content.navigator.teamContent__panel"]/div[2]/div[2]/div')
+        driver.execute_script("arguments[0].scrollTop = 800", scrollable_div)
 
-        # Ahora puedes acceder al div con scroll
-        scrollable_div = driver.find_element(By.CLASS_NAME, "clsViewerPage")
-        driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scrollable_div)
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="com.ibm.bi.content.navigator.teamContent__panel"]/div[2]/div[2]/div/div[1]/div/label[29]/span[2]/div/div[1]/div/a/div/div'))
+        ).click()
+
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="com.ibm.bi.content.navigator.teamContent__panel"]/div[2]/div[2]/div/div[1]/div/label[3]/span[2]/div/div[1]/div/a/div/div'))
+        ).click()
+
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="com.ibm.bi.content.navigator.teamContent__panel"]/div[2]/div[2]/div/div[1]/div/label[2]/span[2]/div/div[1]/div/a/div/div'))
+        ).click()
+
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="com.ibm.bi.content.navigator.teamContent__panel"]/div[2]/div[2]/div/div[1]/div/label[1]/span[2]/div/div[1]/div/a/div/div'))
+        ).click()
+
+        time.sleep(5)  # Esperar manualmente si es necesario
+        
+         # Ahora puedes acceder al div con scroll
+        scrollable_div = driver.find_element(By.XPATH, '//*[@id="com.ibm.bi.content.navigator.teamContent__panel"]/div[2]/div[2]/div')
+        driver.execute_script("arguments[0].scrollTop = 150", scrollable_div)
+
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="com.ibm.bi.content.navigator.teamContent__panel"]/div[2]/div[2]/div/div[2]/div/label[4]/span[2]/div/div[1]/div/a/div/div/div/div'))
+        ).click()
+        time.sleep(50)  # Esperar manualmente si es necesario
+
+        print("1. Esperando el iframe rsIFrameManager_1...")
+        WebDriverWait(driver, 50).until(
+            EC.frame_to_be_available_and_switch_to_it((By.XPATH, '//*[@id="rsIFrameManager_1"]'))
+        )
+        print("2. Cambio al iframe exitoso")
+
+        print("3. Buscando el div scrollable clsViewerPage...")
+        scrollable_div = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "clsViewerPage"))
+        )
+        print(f"4. Div encontrado - Visible: {scrollable_div.is_displayed()} - Tamaño: {scrollable_div.size}")
+
+        print("5. Ejecutando scroll...")
+        driver.execute_script("arguments[0].scrollTop = 400", scrollable_div)
+
+        # Verificación
+        current_scroll = driver.execute_script("return arguments[0].scrollTop", scrollable_div)
+        print(f"6. Scroll completado - Posición actual: {current_scroll}")
 
         time.sleep(5) # Esperar a que se cargue el contenido
 
-        # Hacer clic en el botón de exportar
         WebDriverWait(driver, 90).until(
-            EC.element_to_be_clickable((By.XPATH, '//*[@id="dv8999"]'))
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__17"]'))
+        ).click()
+
+        driver.switch_to.default_content()
+
+        print("1. Esperando el iframe rsIFrameManager_1...")
+        WebDriverWait(driver, 50).until(
+            EC.frame_to_be_available_and_switch_to_it((By.XPATH, '//*[@id="rsIFrameManager_1"]'))
+        )
+        print("2. Cambio al iframe exitoso")
+
+        print("3. Buscando el div scrollable clsViewerPage...")
+        scrollable_div = WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "clsViewerPage"))
+        )
+        print(f"4. Div encontrado - Visible: {scrollable_div.is_displayed()} - Tamaño: {scrollable_div.size}")
+
+        print("5. Ejecutando scroll...")
+        driver.execute_script("arguments[0].scrollTop = 2100", scrollable_div)
+
+        # Verificación
+        current_scroll = driver.execute_script("return arguments[0].scrollTop", scrollable_div)
+        print(f"6. Scroll completado - Posición actual: {current_scroll}")
+
+        time.sleep(5) # Esperar a que se cargue el contenido
+
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__163"]/td/div'))
+        ).click()#Opp Age
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__164"]/td/div'))
+        ).click()#Opportunity Source Name
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__165"]/td/div'))
+        ).click()#Opportunity Source Code
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__166"]/td/div'))
+        ).click()#Opp Owner Name
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__167"]/td/div'))
+        ).click()#Opp Owner Email
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__168"]/td/div'))
+        ).click()#Product Family Name
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__169"]/td/div'))
+        ).click()#Product Family Code
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__170"]/td/div'))
+        ).click()#Classification Name
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__172"]/td/div'))
+        ).click()#Term
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__173"]/td/div'))
+        ).click()#Renewal Type Name
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__174"]/td/div'))
+        ).click()#DS Focus Products
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__175"]/td/div'))
+        ).click()#Owning Org Channel Code
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__176"]/td/div'))
+        ).click()#Owning Org Channel Name
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__177"]/td/div'))
+        ).click()#Sales Motion Name
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__178"]/td/div'))
+        ).click()#Channel Name
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__179"]/td/div'))
+        ).click()#Business Partner Name
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__180"]/td/div'))
+        ).click()#Business Partner Code
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__181"]/td/div'))
+        ).click()#Offering GTM Status
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__182"]/td/div'))
+        ).click()#Sales Competitors Name
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__183"]/td/div'))
+        ).click()#Opp Identifier
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__184"]/td/div'))
+        ).click()#Opp Creator Name
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__185"]/td/div'))
+        ).click()#Opp Next Step
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__186"]/td/div'))
+        ).click()#Type Code
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__187"]/td/div'))
+        ).click()#Type Name
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__188"]/td/div'))
+        ).click()#Deal Size
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__189"]/td/div'))
+        ).click()#SS Last Modified Date
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__190"]/td/div'))
+        ).click()#SS Elapsed Days
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__191"]/td/div'))
+        ).click()#Mon1 Signings Rev
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__192"]/td/div'))
+        ).click()#Mon2 Signings Rev
+        WebDriverWait(driver, 90).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="hal__dom__uniqueID__193"]/td/div'))
+        ).click()#Mon3 Signings Rev
+  
+        driver.switch_to.default_content()
+
+        print("1. Esperando el iframe rsIFrameManager_1...")
+        WebDriverWait(driver, 50).until(
+            EC.frame_to_be_available_and_switch_to_it((By.XPATH, '//*[@id="rsIFrameManager_1"]'))
+        )
+        print("2. Cambio al iframe exitoso")
+
+        print("3. Buscando el div scrollable clsViewerPage...")
+        scrollable_div = WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "clsViewerPage"))
+        )
+        print(f"4. Div encontrado - Visible: {scrollable_div.is_displayed()} - Tamaño: {scrollable_div.size}")
+
+        print("5. Ejecutando scroll...")
+        driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scrollable_div)
+
+        # Verificación
+        current_scroll = driver.execute_script("return arguments[0].scrollTop", scrollable_div)
+        print(f"6. Scroll completado - Posición actual: {current_scroll}")
+
+        time.sleep(30) # Esperar a que se cargue el contenido
+
+        # Hacer clic en el botón de exportar
+        WebDriverWait(driver, 50).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="v8982"]'))
+        ).click()
+
+        time.sleep(100)  # Esperar a que se cargue el contenido
+
+        WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="AppToolbarLeftPane_btnFormats"]'))
+        ).click()
+        time.sleep(5)  # Esperar a que se cargue el contenido
+        WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="mnuAppToolbarLeftPaneFormats_spreadsheetML"]/tbody/tr/td[2]'))
         ).click()
 
         # Verifica que la descarga haya comenzado
-        time.sleep(150)  # Espera corta para que comience la descarga
+        time.sleep(100)  # Espera corta para que comience la descarga
     
         # Verifica que hay archivos de descarga en proceso
         download_started = False
